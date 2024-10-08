0bd9d9f2b3ccbf84fdb82ea9724d109e9dc7b4ef
BabyWaffles <57724817+BabyWaffles@users.noreply.github.com>
2024-10-09 00:33:00 +0800

    Create detestable_QMLUI.py

---
+++ detestable_QMLUI.py
@@ -0,0 +1,9 @@
+from PySide6 import QtCore as qtc
+from PySide6 import QtWidgets as qtw
+from PySide6 import QtGui as qtg
+import sys
+import random
+
+class Windows (qtw.QWidget):
+    def __init__(self):
+        
